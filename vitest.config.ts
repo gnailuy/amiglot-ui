356656eaa52925596595f0d41fb6d386679c85f1
Arturo (Bot) <arturo@gnailuy.com>
2026-02-25 10:31:26 -0800

    Avoid vite-tsconfig-paths to fix CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config";
+import path from "node:path";
+
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
