8b3f4a79182ad95fcf56e2aeca4474b69508630b
Arturo (Bot) <arturo@gnailuy.com>
2026-03-02 18:48:11 -0800

    chore: allow dev origins via env

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: process.env.NEXT_ALLOWED_DEV_ORIGINS
+      ? process.env.NEXT_ALLOWED_DEV_ORIGINS.split(",")
+      : [],
+  },
 };
 
 export default withNextIntl(nextConfig);
